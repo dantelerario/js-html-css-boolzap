f9807cb0457381dbd3d4f0be6965aeb88f96ffa7
Dante L'Erario <dantelerario@gmail.com>
2020-04-17 17:54:31 +0200

    fixing, js missing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 
 $(document).ready( function() {
 
+  var list = $('.main-sent');
+  var keyInput = $('.send-text');
+
+  keyInput.keyup(function(e) {
+    console.log(e.which, e.keyCode);
+
+    if (e.which == 13 || e.keyCode == 13) {
+      var inputList = keyInput.val().trim();
+
+        if (inputList.length > 0) {
+          var newInputList = $('.template li').clone();
+          newInputList.prepend(inputList);
+          list.append(newInputList);
+          keyInput.val('');
+        }
+    }
+  });
+
+  // REMOVE AND UNDERLINE
+  $('body').on('click', '.todo li i', function() {
+    $(this).parent().remove();
+  })
 
 
 
